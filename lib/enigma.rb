abd8f3e8a206f133ebb3c8b1e37d0e9d778346f4
Anthony Blackwell Tallent <anthonytallent567@gmail.com>
2022-11-12 08:17:39 -0800

    make #new_key method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,8 +1,17 @@
+require 'pry'
+
 class Enigma
+  attr_reader :message, :date
+  attr_accessor :key
 
   def initialize(message, key, date)
     @message = message 
     @key = key
     @date = date
+  end
+
+  def new_key
+    @key = '%05d' % rand(5 ** 5)
+    @key.to_i
   end
 end
--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -1,3 +1,4 @@
+require 'pry'
 require './lib/enigma'
 
 RSpec.describe 'Enigma class' do
@@ -5,5 +6,16 @@ RSpec.describe 'Enigma class' do
     enigma = Enigma.new("boo", 02415, "11-11-22")
 
     expect(enigma).to be_a(Enigma)
+  end
+
+  describe '#new_key' do
+    it 'will randomly genrate a new key and assign it to the attribute key' do
+      enigma = Enigma.new("boo", 02415, "11-11-22")
+
+      enigma.new_key
+
+      expect(enigma.key).to be_a(String)
+      expect(enigma.key.length).to eq(5)
+    end
   end
 end
