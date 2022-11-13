242848d2b24ebb38a35c87e4bf3c05cf02830b51
Anthony Blackwell Tallent <anthonytallent567@gmail.com>
2022-11-12 17:56:20 -0800

    Test all aspects of decrypt existence

--- spec/decrypt_spec.rb
+++ spec/decrypt_spec.rb
@@ -8,6 +8,17 @@ RSpec.describe 'Decrypt' do
     decrypt = Decrypt.new('boo hoo', '02415', '11-11-22')
 
     expect(decrypt).to be_a(Decrypt)
+    expect(decrypt.message).to eq("boo hoo")
+    expect(decrypt.key).to eq('02415')
+    expect(decrypt.date).to eq("11-11-22")
+    expect(decrypt.character_set).to eq([
+                                          "a", "b", "c", "d", "e",
+                                          "f", "g", "h", "i", "j", 
+                                          "k", "l", "m", "n", "o", 
+                                          "p", "q", "r", "s", "t", 
+                                          "u", "v", "w", "x", "y", 
+                                          "z", " "
+                                        ])
   end
 
 end
