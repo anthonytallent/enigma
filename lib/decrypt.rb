bc9c57b6e10e85a5fc61ac48c2e0abb1c21828e8
Anthony Blackwell Tallent <anthonytallent567@gmail.com>
2022-11-12 18:13:08 -0800

    Feature #find_message_index_positions

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -9,4 +9,16 @@ class Decrypt < Enigma
     @character_set = ("a".."z").to_a << " "
     super(message, key, date)
   end
+
+  def find_message_index_positions
+    x = []
+    @message.split('').map do |letter|
+      @character_set.each do |character|
+        if letter == character
+          x << @character_set.index(character)
+        end
+      end
+    end
+    x
+  end
 end
