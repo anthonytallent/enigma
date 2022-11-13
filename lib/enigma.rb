f907416e458fa171861667358101f0513c63bd1f
Anthony Blackwell Tallent <anthonytallent567@gmail.com>
2022-11-13 11:07:05 -0800

    Switch #find_message_index_positions to enigma

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -9,5 +9,19 @@ class Enigma < Shift
     @character_set = ("a".."z").to_a << " "
   end
 
+  def find_message_index_positions(message)
+    x = []
+    message.split('').map do |letter|
+      character_set.each do |character|
+        if letter == character
+          x << character_set.index(character)
+        end
+      end
+    end
+    x
+  end
+
   def encrypt(message, key, date)
+
+  end
 end
